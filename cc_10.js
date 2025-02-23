f84197fbcefed54ff95cebb22eb22c27efc03615
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-02-23 18:20:01 -0500

    Task 3 - Created Inventory Class

--- cc_10.js
+++ cc_10.js
@@ -52,3 +52,29 @@ const order1 = new Order(501, prod1, 2);
 // runs the methods in the class and logs it in the console
 console.log(order1.getOrderDetails());
 console.log(prod1.getDetails());
+
+
+// Task 3: Creating an Inventory Class
+
+class Inventory {  // these lines create a new class called Inventory
+    constructor() {
+        // creates an open array (products)
+        this.products = [];
+    };
+
+    addProduct(product){ // creates a method inside the class that will add a product to the products array
+        this.products.push(product);
+    };
+
+    listProducts() { // goes through the products array, runs the getDetails method
+        this.products.forEach(product => console.log(product.getDetails()) );
+    };
+};
+
+// creates a new inventory
+const inventory = new Inventory();
+
+// runs the addProduct method using the parameters given
+inventory.addProduct(prod1);
+// runs the listProducts method after adding a product to the open array
+inventory.listProducts();
